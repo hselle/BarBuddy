8a2973eaff1c768aa89ddda64e0b541aade0a3a6
harry <harrison.selle@coloradocollege.edu>
2018-12-11 19:57:38 +0100

    Functional logic pipeline

--- app/src/main/java/com/example/harrison/barbuddy/MainActivity.kt
+++ app/src/main/java/com/example/harrison/barbuddy/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.harrison.barbuddy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.indexOf
 import android.util.Log
+import android.widget.Toast
 import com.example.harrison.barbuddy.apidata.DetailResult
 import com.example.harrison.barbuddy.apidata.DrinkResult
 import com.example.harrison.barbuddy.apidata.Drinks734794428
@@ -14,34 +16,130 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 class MainActivity : AppCompatActivity() {
     private val HOST_URL = "https://www.thecocktaildb.com/"
+    private var DRINKIDLIST = mutableListOf<String>()
+    private var INGREDIENT_LIST = mutableListOf<String>()
+    private var DRINKDETAILSLIST = mutableListOf<Drinks734794428>()
+    private var DRINK_DICT = hashMapOf<String,List<String>>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val drinkList = listOf("Galliano", "Ginger ale", "Ice")
+        //INGREDIENT_LIST.add("Coffee Liquor")
+        INGREDIENT_LIST.add("Wild Turkey")
+        INGREDIENT_LIST.add("Amaretto")
+        INGREDIENT_LIST.add("Scotch")
+
+        INGREDIENT_LIST.add("Pineapple juice")
+
+
         val retrofit = Retrofit.Builder()
                 .baseUrl(HOST_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
         val drinkAPI = retrofit.create(DrinkAPI::class.java)
+        btnRates.setOnClickListener{
+            val makeables = getMakeableDrinks()
+            Log.w("Hi im daisy", makeables.toString())
+            Toast.makeText(this@MainActivity, "BUTTON RECIEVED", Toast.LENGTH_LONG).show()
+            tvResult.text = makeables.toString()
+        }
+        Thread {
+            createDrinkDict(INGREDIENT_LIST, drinkAPI)
+        }.start()
 
-        val drinkIds = getDrinkIds(drinkList, drinkAPI)
-        val drinkDetailsList = getDrinkDetailsList(drinkIds, drinkAPI)
-        val drinkDict = makeDrinkDictionary(drinkDetailsList)
-        var makeableDrinkList = mutableListOf<String>()
-        drinkDict.keys.forEach { key ->
-            if(checkAllIngredientsInDrink(drinkDict, key, drinkList)){
+    }
+    private fun createDrinkDict(ingredientList: List<String>, drinkAPI: DrinkAPI){
+        ingredientList.forEach{
+            addToDictPipeline(it, drinkAPI)
+        }
+    }
+    fun addToDictPipeline(ingredient: String, drinkAPI: DrinkAPI){
+        val drinkCall = drinkAPI.getDrinks(ingredient)
+        drinkCall.enqueue(object : Callback<DrinkResult> {
+            override fun onFailure(call: Call<DrinkResult>, t: Throwable) {
+                Log.w("Debgg", "Fail" + t.message)
+                tvResult.text = t.message
+            }
+            override fun onResponse(call: Call<DrinkResult>, response: Response<DrinkResult>) {
+                val drinkResult = response.body()
+                Log.w("Debgg", "SUCESS")
+
+                drinkResult?.drinks?.forEach { i ->
+                    Log.w("Debgg", "Found Drink by Ingrediet " + i.idDrink)
+                    drinkToDetailResultPipe(i.idDrink!!, drinkAPI)
+                }
+            }
+        })
+    }
+    fun drinkToDetailResultPipe(drink: String, drinkAPI: DrinkAPI){
+        val drinkCall = drinkAPI.getDetailsById(drink)
+        drinkCall.enqueue(object : Callback<DetailResult> {
+            override fun onFailure(call: Call<DetailResult>, t: Throwable) {
+                Log.w("Debgg", "Fail" + t.message)
+                tvResult.text = t.message
+            }
+
+            override fun onResponse(call: Call<DetailResult>, response: Response<DetailResult>) {
+                val detailResult = response.body()
+                Log.w("Debgg", "SUCESS")
+
+                detailResult?.drinks?.forEach {drink ->
+                    Log.w("Debgg", "Found Drink by Id" + drink.strDrink)
+                    makeDictEntryPipe(drink)
+                }
+            }
+        })
+    }
+    fun makeDictEntryPipe(drink: Drinks734794428){
+        DRINK_DICT[drink.strDrink.toString()] = getAllIngredients(drink)
+        Log.w("Createddbg", "Made drink Dickt entry: " + DRINK_DICT[drink.strDrink.toString()])
+    }
+    private fun getMakeableDrinks(): List<String>{
+        val makeableDrinkList = mutableListOf<String>()
+
+        DRINK_DICT.keys.forEach { key ->
+            if (checkAllIngredientsInDrink(DRINK_DICT, key, INGREDIENT_LIST)) {
                 makeableDrinkList.add(key)
             }
         }
-        Log.w("Debgg", makeableDrinkList.toString())
+        return makeableDrinkList
+    }
+
+    private fun getDrinkDetailsByDrinkId(drinkIds: List<String>, drinkAPI: DrinkAPI){
+        if (drinkIds.isNotEmpty()){
+            getDrinkDetails(drinkIds, drinkAPI, drinkIds[0])
+        }
+    }
+
+    fun getDrinkDetails(drinkIds: List<String>, drinkAPI: DrinkAPI, id: String) {
+        val drinkCall = drinkAPI.getDetailsById(id)
+        drinkCall.enqueue(object : Callback<DetailResult> {
+
+            override fun onFailure(call: Call<DetailResult>, t: Throwable) {
+                Log.w("Debgg", "Fail" + t.message)
+                tvResult.text = t.message
+            }
+
+            override fun onResponse(call: Call<DetailResult>, response: Response<DetailResult>) {
+                val detailResult = response.body()
+                Log.w("Debgg", "SUCESS")
+
+                detailResult?.drinks?.forEach { i ->
+                    Log.w("Debgg", "Found Drink by Id" + i.strDrink)
+                    DRINKDETAILSLIST.add(i)
+                }
+                getDrinkDetailsByDrinkId(drinkIds.slice(1 until drinkIds.size), drinkAPI)
+            }
+        })
     }
 
     private fun checkAllIngredientsInDrink(drinkDict: HashMap<String, List<String>>, key: String, drinkList: List<String>): Boolean {
-        drinkDict.get(key)!!.forEach { ingredient ->
+        drinkDict[key]!!.forEach { ingredient ->
             if (!drinkList.contains(ingredient)) {
                 return false
             }
@@ -50,71 +148,55 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun getDrinkIds(drinkList: List<String>, drinkAPI: DrinkAPI ): MutableList<String>{
-        var drinkIdList = mutableListOf<String>()
-        drinkList.forEach{
-            val drinkCall = drinkAPI.getDrinks(it)
-            drinkCall.enqueue(object : Callback<DrinkResult> {
-                override fun onFailure(call: Call<DrinkResult>, t: Throwable) {
-                    tvResult.text = t.message
-                }
-
-                override fun onResponse(call: Call<DrinkResult>, response: Response<DrinkResult>) {
-                    val drinkResult = response.body()
-                    drinkResult?.drinks?.forEach { i ->
-                        drinkIdList.add(i.idDrink!!.toString())
-                    }
-                }
-            })
-        }
-        return drinkIdList
-    }
     fun getAllIngredients(drink: Drinks734794428): MutableList<String> {
         var allIngredietns = mutableListOf<String>()
-        allIngredietns.add(drink.strIngredient1!!)
-        allIngredietns.add(drink.strIngredient2!!)
-        allIngredietns.add(drink.strIngredient3!!)
-        allIngredietns.add(drink.strIngredient4!!)
-        allIngredietns.add(drink.strIngredient5!!)
-        allIngredietns.add(drink.strIngredient6!!)
-        allIngredietns.add(drink.strIngredient7!!)
-        allIngredietns.add(drink.strIngredient8!!)
-        allIngredietns.add(drink.strIngredient9!!)
-        allIngredietns.add(drink.strIngredient10!!)
-        allIngredietns.add(drink.strIngredient11!!)
-        allIngredietns.add(drink.strIngredient12!!)
-        allIngredietns.add(drink.strIngredient13!!)
-        allIngredietns.add(drink.strIngredient14!!)
-        allIngredietns.add(drink.strIngredient15!!)
+        if(drink.strIngredient1 != ""){
+            allIngredietns.add(drink.strIngredient1.toString())
+        }
+        if(drink.strIngredient2 != ""){
+            allIngredietns.add(drink.strIngredient2.toString())
+        }
+        if(drink.strIngredient3 != ""){
+            allIngredietns.add(drink.strIngredient3.toString())
+        }
+        if(drink.strIngredient4 != ""){
+            allIngredietns.add(drink.strIngredient4.toString())
+        }
+        if(drink.strIngredient5 != ""){
+            allIngredietns.add(drink.strIngredient5.toString())
+        }
+        if(drink.strIngredient6 != ""){
+            allIngredietns.add(drink.strIngredient6.toString())
+        }
+        if(drink.strIngredient7 != ""){
+            allIngredietns.add(drink.strIngredient7.toString())
+        }
+        if(drink.strIngredient8 != ""){
+            allIngredietns.add(drink.strIngredient8.toString())
+        }
+        if(drink.strIngredient9 != ""){
+            allIngredietns.add(drink.strIngredient9.toString())
+        }
+        if(drink.strIngredient10 != ""){
+            allIngredietns.add(drink.strIngredient10.toString())
+        }
+        if(drink.strIngredient11 != ""){
+            allIngredietns.add(drink.strIngredient11.toString())
+        }
+        if(drink.strIngredient12 != ""){
+            allIngredietns.add(drink.strIngredient12.toString())
+        }
+        if(drink.strIngredient13 != ""){
+            allIngredietns.add(drink.strIngredient13.toString())
+        }
+        if(drink.strIngredient14 != ""){
+            allIngredietns.add(drink.strIngredient14.toString())
+        }
+        if(drink.strIngredient15 != ""){
+            allIngredietns.add(drink.strIngredient15.toString())
+        }
         return allIngredietns
-
     }
 
-    private fun makeDrinkDictionary(drinkDetailsList: List<DetailResult>): HashMap<String, List<String>> {
-        var drinkIngredientDict = hashMapOf<String, List<String>>()
-        drinkDetailsList.forEach {
-            it.drinks?.forEach {
-                drinkIngredientDict.put(it.strDrink!!, getAllIngredients(it))
-            }
-        }
-        return drinkIngredientDict
-    }
 
-    private fun getDrinkDetailsList(drinkIds: MutableList<String>, drinkAPI: DrinkAPI): List<DetailResult> {
-        var drinkDetailsList = mutableListOf<DetailResult>()
-        drinkIds.forEach {
-            val drinkDetailsCall = drinkAPI.getDetailsById(it)
-            drinkDetailsCall.enqueue(object : Callback<DetailResult> {
-                override fun onFailure(call: Call<DetailResult>, t: Throwable) {
-                    tvResult.text = t.message
-                    Log.w("API_ERROR", "Api error in details call")
-                }
-
-                override fun onResponse(call: Call<DetailResult>, response: Response<DetailResult>) {
-                    drinkDetailsList.add(response.body()!!)
-                }
-            })
-        }
-        return drinkDetailsList
-    }
 }

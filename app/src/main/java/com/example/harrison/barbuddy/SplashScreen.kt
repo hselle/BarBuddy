33d70e13dbc682b28473569b7a496cd7a8d370f8
gpinkard <gbrlpnkrd@gmail.com>
2018-12-12 15:16:59 +0100

    anim

--- app/src/main/java/com/example/harrison/barbuddy/SplashScreen.kt
+++ app/src/main/java/com/example/harrison/barbuddy/SplashScreen.kt
@@ -1,12 +1,33 @@
 package com.example.harrison.barbuddy
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
-    }
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_splash_screen)
+
+            val anim = AnimationUtils.loadAnimation(this, R.anim.splash_screen_anim)
+            anim.setAnimationListener(object : Animation.AnimationListener {
+
+                override fun onAnimationRepeat(animation: Animation?) {
+                }
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    val mainIntent = Intent(this@SplashScreen, MainActivity::class.java)
+                    this@SplashScreen.startActivity(mainIntent)
+                    this@SplashScreen.finish()
+                }
+
+                override fun onAnimationStart(animation: Animation?) {
+                }
+            })
+            ivSplashScreen.startAnimation(anim)
+        }
 }
